6ee06acc84cfbb259b93e96d5ab8dd7f93a68110
debsdaniel <dbojorges@gmail.com>
2020-05-11 18:41:34 -0600

    ORM - 2

--- ORM/orm1.py
+++ ORM/orm1.py
@@ -18,3 +18,36 @@ m = Marca.objects.filter(~Q(descripcion__startswith='D'))
 
 m = Marca.objects.filter(~Q(descripcion__startswith='D') & ~Q(descripcion__startswith='d'))
 
+m = Marca.objects.all().exclude(id=1)
+m = Marca.objects.filter(descripcion__startswith='D').exclude(id=1)
+
+# Mostrar ciertos campos
+m = Marca.objects.filter(descripcion__startswith='D').values('descripcion')
+
+# Sub Consultas
+from django.db.models import Subquery
+
+m = Marca.objects.filter(id__in=Subquery(Categoria.objects.values('id')))
+
+
+
+select * from marca where id in (select id from categoria)
+
+
+# INNER JOIN
+sc = Subcategoria.objects.filter(categoria__id=7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
